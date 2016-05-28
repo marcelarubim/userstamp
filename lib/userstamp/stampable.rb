0917f0e1b361adc262e3cf150c77efe2f2d2f8be
Eddy Kim <eddyhkim@gmail.com>
2016-05-28 12:50:47 -0700

    Quick hack to avoid mixing stampable into AR::Base, which causes the doubly defined assocation errors in  rails 4.1

--- lib/userstamp/stampable.rb
+++ lib/userstamp/stampable.rb
@@ -66,6 +66,7 @@ module Ddb #:nodoc:
         # The method will automatically setup all the associations, and create <tt>before_save</tt>
         # and <tt>before_create</tt> filters for doing the stamping.
         def stampable(options = {})
+          return if self == ActiveRecord::Base
           defaults  = {
                         :stamper_class_name => :user,
                         :creator_attribute  => Ddb::Userstamp.compatibility_mode ? :created_by : :creator_id,
